8c2effa781a95309c53b6ea1da79b39241e42104
Doron Serebro <doronser@gmail.com>
2020-04-10 14:39:02 +0300

    finished the sfotmax part

--- Assignment1/cs231n/classifiers/softmax.py
+++ Assignment1/cs231n/classifiers/softmax.py
@@ -33,10 +33,36 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    for i in range(num_train):
+      f = X[i] @ W 
+      correct_class_score =  f[y[i]]
+
+      f_norm = f - np.max(f) # normalize to avoid numeric errors (due to division of big exponents)
+      p =  np.exp(f_norm) / np.sum(np.exp(f_norm)) # apply the softmax function 
+
+      loss -= correct_class_score
+      dW[:,y[i]] += (p[y[i]]-1) * X[i]
+      
+      exp_sum = 0
+      for j in range(num_classes):
+        exp_sum += np.exp(f[j])
+        if j==y[i]:
+          continue
+        dW[:,j] += p[j] * X[i]
+        
+                
+      loss += np.log(exp_sum)
+
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+
+    dW /= num_train
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
+    
     return loss, dW
 
 
@@ -57,8 +83,19 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    scores = X @ W
+    scores -= np.max(scores)
+    P = np.exp(scores) / np.sum(np.exp(scores),axis=1).reshape(num_train,1)
+    help_mat = np.zeros_like(P)
+    help_mat[np.arange(num_train),y] = 1
+    loss = np.mean( -np.log(P[np.arange(num_train),y]))
+    loss += reg * np.sum(W * W)
 
-    pass
+    dW = X.T @ (P - help_mat)
+    dW /= num_train
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
